960d0192558427c0a92aeb27bb55ecd734ee9006
Andrew Hesford <ajh@lower.sideband.org>
2009-09-30 16:14:19 -0400

    Remove all cexp calls in favor of cos and sin.

--- fsgreen.c
+++ fsgreen.c
@@ -6,7 +6,7 @@
 /* Computes the free-space Green's function between two points. */
 complex float fsgreen (float k, float *r, float *rp) {
 	complex float ans;
-	float dist;
+	float dist, kdist;
 
 	/* Compute the distance between elements. */
 	dist = (r[0] - rp[0]) * (r[0] - rp[0]);
@@ -14,7 +14,9 @@ complex float fsgreen (float k, float *r, float *rp) {
 	dist += (r[2] - rp[2]) * (r[2] - rp[2]);
 	dist = sqrt (dist);
 
-	ans = cexp (I * k * dist) / (4 * M_PI * dist);
+	kdist = k * dist;
+
+	ans = (cos (kdist) + I * sin (kdist))  / (4 * M_PI * dist);
 
 	return ans;
 }
@@ -22,12 +24,14 @@ complex float fsgreen (float k, float *r, float *rp) {
 /* Computes a plane wave from a specific direction at a point. */
 complex float fsplane (float k, float *r, float *s) {
 	complex float ans;
-	float sr, ds;
+	float sr, ds, ksr;
 
 	ds = sqrt(s[0] * s[0] + s[1] * s[1] + s[2] * s[2]);
 	sr = (s[0] * r[0] + s[1] * r[1] + s[2] * r[2]) / ds;
 
-	ans = cexp (-I * k * sr) / (4 * M_PI);
+	ksr = k * sr;
+
+	ans = (cos (ksr) - I * sin (ksr)) / (4 * M_PI);
 
 	return ans;
 }
